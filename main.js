eb59fc10d949bcc014ff61236d8359002ef53028
Shodmonov2006 <olimkhujashodmonovv@gmail.com>
2023-04-12 15:23:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+// >>>>>>>>>>HOME TASK<<<<<<<<<<//
+// 1
+// function circuitPower(a,b){
+//     return a*b  
+// }
+// console.log(circuitPower(110, 3));  
+
+
+// 2
+// function divisibleByFive(a){
+//     return a%5==0
+// }
+// console.log(divisibleByFive(54));
+
+
+// 3
+// function isSameNum(a, b){
+//     return a==b
+// }
+// console.log(isSameNum(2, 2));
+
+
+// 4
+// function longestTime(a,b,c){
+//     let hour = a*3600
+//     let minute = b*60
+//     let second = c*1
+//     if(hour>minute && hour>second){
+//         return a
+//     }
+//     else if(minute>hour && minute>second){
+//         return b
+//     }
+//     else{
+//         return c
+//     }
+// }
+// console.log(longestTime(1, 59, 3601));
+
+
+// 5
+// function oneOddOneEven(a){
+//     let oddorEven = Math.floor(a%10)
+//     let oddorEven2 = Math.floor(a/10%10)
+//     if(oddorEven%2==0 && oddorEven2&2!=0 || oddorEven%2!=0 && oddorEven2&2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(oneOddOneEven(56))
+
+
+// 6
+// function negativeNum(a){
+//     if(a>0){
+//         return -a
+//     }
+//     else{
+//         return a
+//     }
+// }
+// console.log(negativeNum(55));
